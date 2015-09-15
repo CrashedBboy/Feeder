5f1cc8d86dc730620a56e8ffd434a3bf4973ad06
CrashedBboy <crashedbboy@gmail.com>
2015-09-15 11:05:05 +0800

    Feed depend on time

--- Feeder.ino
+++ Feeder.ino
@@ -8,7 +8,7 @@ const int kCePin   = 5;  // Chip Enable
 const int kIoPin   = 6;  // Input/Output
 const int kSclkPin = 7;  // Serial Clock
 const int servoPin = 9;  // Servo
-const int tip120pin = 8;
+const int tip120pin = 8; // transistor
 
 DS1302 rtc(kCePin, kIoPin, kSclkPin);
 
@@ -25,12 +25,9 @@ String dayAsString(const Time::Day day) {
   return "(unknown day)";
 }
 
-
-
 void printTime() {
   Time t = rtc.time();
   const String day = dayAsString(t.day);
-
   char buf[50];
   snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
            day.c_str(),
@@ -39,20 +36,18 @@ void printTime() {
   Serial.println(buf);
 }
 
-void setup() {
-  Serial.begin(9600);
-  
-  pinMode(tip120pin, OUTPUT);
-  analogWrite(tip120pin, 0); 
-
+void setTime(int year,int month,int day,int hour,int min,int sec, Time::Day weekday) {  //setTime(2014, 9, 13, 13, 55, 00, Time::kSunday);
   rtc.writeProtect(false);
   rtc.halt(false);
+  Time t(year, month, day, hour, min, sec, weekday);
+  rtc.time(t);
+}
 
-  //Time t(2014, 9, 13, 13, 55, 00, Time::kSunday);
-  //rtc.time(t);
+void setup() {
+  Serial.begin(9600);
+  pinMode(tip120pin, OUTPUT);
+  analogWrite(tip120pin, 0); 
   myservo.attach(servoPin);
-  feed();
-  feed();
 }
 
 void feed(){
@@ -60,12 +55,15 @@ void feed(){
   myservo.write(180);
   delay(1500);
   myservo.write(0);
-  delay(3000);
+  delay(1500);
   analogWrite(tip120pin, 0);
 }
 
-// Loop and print the time every second.
 void loop() {
-  printTime();
+  //printTime();
+  Time counter = rtc.time();
+  if (counter.sec == 0 && counter.min == 0 ) {
+    feed();  
+  }
   delay(1000);
 }
