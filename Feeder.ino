f4aeb1b9c4d204af4c581c20893be5d01439df8e
CrashedBboy <crashedbboy@gmail.com>
2015-09-15 12:31:07 +0800

    clean trash

--- Feeder.ino
+++ Feeder.ino
@@ -1,36 +1,49 @@
 #include <stdio.h>
-#include "DS1302.h"
 #include <Servo.h>
+#include "DS1302.h"
+
 Servo myservo;
-
-
 const int kCePin   = 5;  // Chip Enable
 const int kIoPin   = 6;  // Input/Output
 const int kSclkPin = 7;  // Serial Clock
 const int servoPin = 9;  // Servo
 const int tip120pin = 8; // transistor
-
 DS1302 rtc(kCePin, kIoPin, kSclkPin);
 
-String dayAsString(const Time::Day day) {
-  switch (day) {
-    case Time::kSunday: return "Sunday";
-    case Time::kMonday: return "Monday";
-    case Time::kTuesday: return "Tuesday";
-    case Time::kWednesday: return "Wednesday";
-    case Time::kThursday: return "Thursday";
-    case Time::kFriday: return "Friday";
-    case Time::kSaturday: return "Saturday";
-  }
-  return "(unknown day)";
+void setup() {
+  Serial.begin(9600);
+  pinMode(tip120pin, OUTPUT);
+  analogWrite(tip120pin, 0); 
+  myservo.attach(servoPin);
 }
 
+void loop() {
+  Time counter = rtc.time();
+  if (counter.sec == 0 && counter.min == 0 ) {
+    feed(1);  
+  }
+  delay(1000);
+}
+
+/* Control modules */
+/*-----------------------------------------------------------------------*/
+void feed(int amount){
+  analogWrite(tip120pin, 255);
+  for (int i = 0; i < amount; i++) {
+    myservo.write(180);
+    delay(1500);
+    myservo.write(0);
+    delay(1500);
+  }
+  analogWrite(tip120pin, 0);
+}
+
+/* Time and Date */
+/*-----------------------------------------------------------------------*/
 void printTime() {
   Time t = rtc.time();
-  const String day = dayAsString(t.day);
-  char buf[50];
-  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
-           day.c_str(),
+  char buf[24];
+  snprintf(buf, sizeof(buf), "%04d-%02d-%02d %02d:%02d:%02d",
            t.yr, t.mon, t.date,
            t.hr, t.min, t.sec);
   Serial.println(buf);
@@ -41,29 +54,4 @@ void setTime(int year,int month,int day,int hour,int min,int sec, Time::Day week
   rtc.halt(false);
   Time t(year, month, day, hour, min, sec, weekday);
   rtc.time(t);
-}
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(tip120pin, OUTPUT);
-  analogWrite(tip120pin, 0); 
-  myservo.attach(servoPin);
-}
-
-void feed(){
-  analogWrite(tip120pin, 255);
-  myservo.write(180);
-  delay(1500);
-  myservo.write(0);
-  delay(1500);
-  analogWrite(tip120pin, 0);
-}
-
-void loop() {
-  //printTime();
-  Time counter = rtc.time();
-  if (counter.sec == 0 && counter.min == 0 ) {
-    feed();  
-  }
-  delay(1000);
 }
