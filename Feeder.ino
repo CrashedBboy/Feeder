27cfa10d791b8dc6a6e63cf95c8a4f8f504aed33
CrashedBboy <crashedbboy@gmail.com>
2015-11-17 03:44:12 +0800

    tcp connection

--- Feeder.ino
+++ Feeder.ino
@@ -1,32 +1,60 @@
+#include <EtherCard.h>
 #include <stdio.h>
 #include <Servo.h>
-#include "DS1302.h"
+byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
+static byte session;
+
+byte Ethernet::buffer[700];
+Stash stash;
+
+static void reqSend () {
+  Serial.println("Starting connection...");
+
+  Stash::prepare(PSTR("Hello I'm Feeder"));
+
+  session = ether.tcpSend();
+}
 Servo myservo;
-const int kCePin   = 2;  // Chip Enable
-const int kIoPin   = 3;  // Input/Output
-const int kSclkPin = 4;  // Serial Clock
-const int servoPin = 5;  // Servo
-const int tip120pin = 6; // transistor
-DS1302 rtc(kCePin, kIoPin, kSclkPin);
+const int servoPin = 5;  // Servo (SG 90)
+const int tip120pin = 6; // transistor (TIP 120)
 
 void setup() {
   Serial.begin(9600);
   pinMode(tip120pin, OUTPUT);
   analogWrite(tip120pin, 0); 
   myservo.attach(servoPin);
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+    Serial.println(F("Failed to access Ethernet controller"));
+  if (!ether.dhcpSetup())
+    Serial.println(F("DHCP failed"));
+    
+  ether.hisip[0] = 140;
+  ether.hisip[1] = 115;
+  ether.hisip[2] = 189;
+  ether.hisip[3] = 142;
+  ether.hisport = 3001;
+  ether.persistTcpConnection(true);
+  reqSend();
+  //feed(1); 
 }
 
 void loop() {
-  printTime();
-  Time counter = rtc.time();
-  if (counter.sec == 0 && counter.min == 0 ) {
-    feed(1);  
+  ether.packetLoop(ether.packetReceive());
+
+  const char* reply = ether.tcpReply(session);
+  if (reply != 0) {
+    Serial.println("Got a response!");
+    Serial.println(reply);
+    if(reply[0] == 'f') {
+      Serial.println("Got it!");
+      feed(1);
+    }
   }
-  delay(1000);
 }
 
-/* Control modules */
+/* Control Servo */
 /*-----------------------------------------------------------------------*/
 void feed(int amount){
   analogWrite(tip120pin, 255);
@@ -37,22 +65,4 @@ void feed(int amount){
     delay(1500);
   }
   analogWrite(tip120pin, 0);
-}
-
-/* Time and Date */
-/*-----------------------------------------------------------------------*/
-void printTime() {
-  Time t = rtc.time();
-  char buf[24];
-  snprintf(buf, sizeof(buf), "%04d-%02d-%02d %02d:%02d:%02d",
-           t.yr, t.mon, t.date,
-           t.hr, t.min, t.sec);
-  Serial.println(buf);
-}
-
-void setTime(int year,int month,int day,int hour,int min,int sec, Time::Day weekday) {  //setTime(2014, 9, 13, 13, 55, 00, Time::kSunday);
-  rtc.writeProtect(false);
-  rtc.halt(false);
-  Time t(year, month, day, hour, min, sec, weekday);
-  rtc.time(t);
 }
